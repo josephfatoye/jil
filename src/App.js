81f058aa4cbfe9118f086c984b218314bc2847ab
PAPPY-JOE <fatoyejoseph@gmail.com>
2024-06-14 00:33:33 +0100

    JIL styling

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p style={{
+          fontWeight: 'bold',
+        }}>
           JIL - Team X
         </p>
         <p style={{
