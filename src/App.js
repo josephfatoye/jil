6f963b291c6154dc9204ee8c6393a784bebe4fc3
PAPPY-JOE <fatoyejoseph@gmail.com>
2024-06-14 22:59:15 +0100

    updated src

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world!</div>,
+    },
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p style={{
-          fontWeight: 'bold',
-        }}>
-          JIL - Team X
-          <br />
-          Members</p>
-        <ul style={{
-          textAlign: 'left'
-        }}>
-          <li>Mofi</li>
-          <li>Precious</li>
-          <li>Joshua</li>
-          <li>Goodness</li>
-          <li>Joseph</li>
-        </ul>
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
 }
