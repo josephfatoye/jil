a6f24e439e83a3699a16ee002e754f2d44074e47
PAPPY-JOE <fatoyejoseph@gmail.com>
2024-06-17 12:05:50 +0100

    Implemented Auth

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,93 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { RiMenu5Fill } from "react-icons/ri";
 import { IoFastFoodSharp } from "react-icons/io5";
+import { UserAuth } from "../context/Auth";
+import { useNavigate } from "react-router-dom";
+import { Avatar } from "@chakra-ui/react";
+import { signInWithPopup, signOut, GoogleAuthProvider } from "firebase/auth";
+import { auth } from "../firebase";
+import { HashLink } from "react-router-hash-link";
 
 function Header() {
+  const navigate = useNavigate();
+
   const [menuToggle, setMenuToggle] = useState(false);
   const handleToggle = () => setMenuToggle(!menuToggle);
+
+  const { user } = UserAuth();
+  const [loading, setLoading] = useState(true);
+
+  const handleSignIn = async () => {
+    handleToggle();
+    try {
+      const provider = new GoogleAuthProvider();
+      await signInWithPopup(auth, provider).then((res) => {
+        console.log(res);
+        navigate("/dashboard");
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSignOut = async () => {
+    handleToggle();
+    try {
+      await signOut(auth);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const checkAuthentication = async () => {
+      await new Promise((resolve) => setTimeout(resolve, 50));
+      setLoading(false);
+    };
+    checkAuthentication();
+  }, [user]);
+
   return (
     <div className="bg-white bg-opacity-95 fixed font-medium z-10 w-full p-2 flex flex-row items-center justify-between px-4 md:px-12">
-      <p className="text-3xl flex items-center md:text-5xl text-orange-400 md:w-full">
+      <p
+        className="text-3xl flex items-center md:text-5xl text-orange-400 md:w-full cursor-pointer"
+        onClick={() => navigate("/")}
+      >
         <IoFastFoodSharp />{" "}
         <i className="text-green-400 text-2xl md:text-4xl md:flex-1 md:text-center md:w-full">
           J<span className="text-orange-400">i</span>L
         </i>
       </p>
       <div className="hidden md:flex flex-row items-center justify-center w-max flex-1 whitespace-nowrap space-x-12">
-        <p className="text-sm text-black hover:text-orange-400 cursor-pointer">
+        <p
+          className="text-sm text-black hover:text-orange-400 cursor-pointer"
+          onClick={() => navigate("/#about")}
+        >
           About
         </p>
-        <p className="text-sm text-black hover:text-orange-400 cursor-pointer">
+        <p
+          className="text-sm text-black hover:text-orange-400 cursor-pointer"
+          onClick={() => navigate("/#blog")}
+        >
           Blog
         </p>
-        <p className="text-sm bg-green-500 hover:bg-green-400 active:bg-green-300 text-white p-2 rounded-lg cursor-pointer">
-          Get Started
-        </p>
+        {!loading && user && (
+          <div>
+            <Avatar name={user.displayName} src={user.photoURL ?? ""} />
+          </div>
+        )}
+        {!loading && (
+          <p
+            onClick={user ? handleSignOut : handleSignIn}
+            className={`text-sm text-white ${
+              user
+                ? "bg-red-500 hover:bg-red-400 active:bg-red-300"
+                : "bg-green-500 hover:bg-green-400 active:bg-green-300"
+            }  p-2 rounded-lg cursor-pointer`}
+          >
+            {user ? "Sign Out" : "Get Started"}
+          </p>
+        )}
       </div>
       <div className="flex items-center justify-center md:hidden">
         <RiMenu5Fill
@@ -38,15 +103,42 @@ function Header() {
             className="absolute backdrop-blur w-screen h-screen"
           />
           <div className="z-50 flex flex-col items-center justify-center space-y-12 my-4 bg-white p-12 rounded-2xl">
-            <p className="text-green-400 text-base cursor-pointer font-medium">
+            <HashLink
+              className="text-sm text-black hover:text-orange-400 cursor-pointer"
+              to={"/#about"}
+            >
+              About
+            </HashLink>
+            <HashLink
+              className="text-sm text-black hover:text-orange-400 cursor-pointer"
+              to={"/#blog"}
+            >
+              Blog
+            </HashLink>
+            <p
+              className="text-green-400 text-base cursor-pointer font-medium"
+              onClick={() => navigate("/#about")}
+            >
               About
             </p>
-            <p className="text-green-400 text-base cursor-pointer font-medium">
+            <p
+              className="text-green-400 text-base cursor-pointer font-medium"
+              onClick={() => navigate("/#blog")}
+            >
               Blog
             </p>
-            <p className=" text-base cursor-pointer font-medium bg-green-400 text-white p-2 rounded-lg">
-              Get Started
-            </p>
+            {!loading && (
+              <p
+                onClick={user ? handleSignOut : handleSignIn}
+                className={`text-base cursor-pointer font-medium ${
+                  user
+                    ? "bg-red-500 hover:bg-red-400 active:bg-red-300"
+                    : "bg-green-500 hover:bg-green-400 active:bg-green-300"
+                } text-white p-2 rounded-lg`}
+              >
+                {user ? "Sign Out" : "Get Started"}
+              </p>
+            )}
           </div>
         </div>
       )}
