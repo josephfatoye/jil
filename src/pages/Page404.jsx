fd30e3ce917c9adfad1f35977a36a568a99b96ad
PAPPY-JOE <fatoyejoseph@gmail.com>
2024-06-17 12:15:23 +0100

    updates

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Page404() {
-  return <div>Page404</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="mt-20 min-h-sceen flex items-center">
+      <p className="text-red-500 font-medium text-3xl">
+        This page does not exist.
+      </p>
+      <p
+        onClick={navigate("/")}
+        className='text-sm text-white "bg-green-500 hover:bg-green-400 active:bg-green-300" p-2 rounded-lg cursor-pointer'
+      >
+        Return Home
+      </p>
+    </div>
+  );
 }
 
 export default Page404;
